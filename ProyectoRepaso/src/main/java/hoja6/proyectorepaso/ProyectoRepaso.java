7eb7343190bb8785511d334a27cfa51b21d9d885
ivaandiaz <ivan.diazfernandez@iesmiguelherrero.com>
2022-12-23 18:49:28 +0100

    Primer comit

---
+++ ProyectoRepaso/src/main/java/hoja6/proyectorepaso/ProyectoRepaso.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package hoja6.proyectorepaso;
+
+/**
+ *
+ * @author DAM110
+ */
+public class ProyectoRepaso {
+
+    public static void main(String[] args) {
+        System.out.println("Primera linea");
+    }
+}
